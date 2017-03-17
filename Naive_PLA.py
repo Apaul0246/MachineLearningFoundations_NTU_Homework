9c07ee9bb390a9728c76d60d6c3c5351894e7d44
Apaul0246 <boya.you1@gmail.com>
2017-03-17 11:57:59 -0500

    added Naive_PLA.py

---
+++ Naive_PLA.py
@@ -0,0 +1,39 @@
+'''
+Python code - Naive PLA
+Data: https://www.csie.ntu.edu.tw/~htlin/course/ml15fall/hw1/hw1_15_train.dat
+
+'''
+
+import numpy as np
+import pandas as pd
+
+def read_data(path):
+	
+    df = pd.read_csv(path,sep='\s+',header=None)
+    x0 = np.ones((len(df.index),1))
+    df.insert(0,'x0',x0)
+    return df
+	
+def PLA_naive(df):
+
+	w0 = np.zeros((len(df.columns)-1))
+	update = 0
+	New_Y = np.zeros((len(df.index)))
+	
+	while (sum(New_Y != df.iloc[:,-1].tolist())>0):
+		for i in range(len(df.index)):
+			if sum(New_Y != df.iloc[:,-1].tolist())==0:
+				print 'Best separating hyperplane found'
+				print w0
+				break 
+			if New_Y[i] == df.iloc[:,-1][i]:
+				pass
+			else:
+				w0 += np.dot(df.iloc[:,-1][i],df.iloc[i,:-1])
+				New_Y = np.sign(np.dot(w0,df.iloc[:,:-1].T))
+				update += 1
+				print '%d update' %(update),'update the %d data' %(i)
+	return w0
+
+df = read_data("C:\Users\user\OneDrive\Code\hw1_15_train.dat")
+w = PLA_naive(df)
