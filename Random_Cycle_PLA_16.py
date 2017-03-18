e92168a57982302e2b536cc1c1e7cbb6de772a3d
Apaul0246 <boya.you1@gmail.com>
2017-03-18 15:45:19 -0500

    answer to h1q16

---
+++ Random_Cycle_PLA_16.py
@@ -0,0 +1,51 @@
+'''
+Python code - Naive PLA by visiting examples in fixed pre-determined random cycles
+Data: https://www.csie.ntu.edu.tw/~htlin/course/ml15fall/hw1/hw1_15_train.dat
+Packages required: Numpy, Pandas
+'''
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+def read_data(path):
+    df = pd.read_csv(path,sep='\s+',header=None)
+    x0 = np.ones((len(df.index),1))
+    df.insert(0,'x0',x0)
+    return df
+
+def PLA_naive(df):
+
+	w0 = np.zeros((len(df.columns)-1))
+	update = 0
+	New_Y = np.zeros((len(df.index)))
+	
+	dataindex = list(range(len(df.index)))
+	np.random.shuffle(dataindex)
+
+	while (sum(New_Y != df.iloc[:,-1])>0):
+		for i in range(len(df.index)):
+			if sum(New_Y != df.iloc[:,-1]) == 0:
+				break 
+			if New_Y[dataindex[i]] == df.iloc[:,-1][dataindex[i]]:
+				pass
+			else:
+				w0 += np.dot(df.iloc[:,-1][dataindex[i]],df.iloc[dataindex[i],:-1])
+				New_Y = np.sign(np.dot(w0,df.iloc[:,:-1].T))
+				update += 1
+	return update,w0
+
+def run_PLA(df,t):
+	update_set = []
+	w_set = []
+	for i in range(t):
+		update,w = PLA_naive(df)
+		update_set.append(update)
+		w_set.append(w)
+	sns.distplot(update_set)
+	sns.plt.show()
+	print np.average(update_set)
+
+df = read_data("C:\Users\user\OneDrive\Code\hw1_15_train.dat")
+run_PLA(df,2000)
+
